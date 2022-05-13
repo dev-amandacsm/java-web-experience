59cf588e12ccd6963a615e0f32020dffc3cbc713
Amanda Muniz <amandacsm@ibm.com>
2022-05-12 23:28:49 -0300

    Primeiro commit

---
+++ src/main/java/com/fatec/java/web/experience/controllers/HomeServlet.java
@@ -0,0 +1,103 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package com.fatec.java.web.experience.controllers;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "HomeServlet", urlPatterns = {"/"})
+public class HomeServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try ( PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet HomeServlet</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<form>");
+            out.println("<h2>Insira um texto</h2><br>");
+            out.println("<input type='text' name='txtMessage'>");
+            out.println("<input type='submit' name='btnSubmit'>");
+            out.println("</form>");
+            out.println("<br>");
+            String message = request.getParameter("txtMessage");
+            if(message != null && message.length() > 0){
+                out.println(String.format("<h5>Texto informado: %s</h5>", message));
+                out.println(String.format("<h5>Texto maiúsculo: %s</h5>", getUpperCase(message)));
+                out.println(String.format("<h5>Texto minúsculo: %s</h5>", getLowerCase(message)));
+            }
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+    
+    private String getUpperCase(String text){
+        return text.toUpperCase();
+    }
+    
+    private String getLowerCase(String text){
+        return text.toLowerCase();
+    }
+    
+    
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
